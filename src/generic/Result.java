4e60f370ffae3878ff0d24c1e5a2f704ab1f9889
Sachin <sachins@boston-technology.com>
2019-01-04 07:20:03 -0500

    Updated Retry listeners to Result class.Retry Listeners will listen to the failed test scripts and re-execute those again.

--- src/generic/Result.java
+++ src/generic/Result.java
@@ -1,13 +1,22 @@
 package generic;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+import org.testng.IRetryAnalyzer;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 import org.testng.Reporter;
+import org.testng.annotations.ITestAnnotation;
+import org.testng.internal.annotations.IAnnotationTransformer;
 
-public class Result implements ITestListener, IAutoConst{
+public class Result implements ITestListener, IAutoConst, IAnnotationTransformer, IRetryAnalyzer{
 
 	static int passCount=0,failCount=0;
+	private int retryCnt = 0;
+    private int maxRetryCnt = 2;
+    
 	@Override
 	public void onTestSuccess(ITestResult result) {
 		String name = result.getName();
@@ -51,5 +60,24 @@ public class Result implements ITestListener, IAutoConst{
 	public void onTestStart(ITestResult result) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	@Override
+	public boolean retry(ITestResult result) {
+		if (retryCnt < maxRetryCnt) {
+            System.out.println("Retrying " + result.getName() + " again and the count is " + (retryCnt+1));
+            retryCnt++;
+            return true;
+        }
+		return false;
+	}
+	
+	@Override
+	public void transform(ITestAnnotation testannotation, Class testClass, Constructor testConstructor, Method testMethod) {
+		IRetryAnalyzer retry1 = testannotation.getRetryAnalyzer();
+
+		if (retry1 == null)	{
+			testannotation.setRetryAnalyzer(Result.class);
+		}	
 	}
 }
